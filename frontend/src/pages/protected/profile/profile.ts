312711beb4b54c8c46697027d59ef3e00e1075cf
SimplyPowfu <carbonaroenricog@gmail.com>
2026-01-30 16:21:07 +0100

    [MINOR] fix profile page

--- frontend/src/pages/protected/profile/profile.ts
+++ frontend/src/pages/protected/profile/profile.ts
@@ -4,16 +4,18 @@ import { generateInitialsAvatar } from "@/components/createDefaultImage";
 
 import { GameData, createHistoryBar } from "@/components/createHistoryBar";
 import { createFriendsBar } from "@/components/createFriendBar";
-import { sendPostRequest, sendPatchRequest } from "@/services/api/sendRequests";
+import { sendPatchRequest } from "@/services/api/sendRequests";
 import { router } from "@/router";
 
 let paramUsername:string = '';
 
-export function loadProfilePage(): HTMLElement {
+export async function loadProfilePage(): Promise<HTMLElement> {
 
 	const params = router.getParams();
 	paramUsername = params.username;
-	const routeUsername = router.getCurrentRoute()?.meta?.user?.username;
+	const me = await sendGetRequest(`/api/isauth`);
+	if (me.ok === false) throw new Error('No authentication token found');
+	if (!me.user.username) throw new Error('username not found')
 
 	// overflow-hidden whitespace-nowrap text-ellispis
 
@@ -59,7 +61,7 @@ export function loadProfilePage(): HTMLElement {
 				<div class="ml-8 text-left flex-grow">
 					<div class="flex items-center justify-between mb-4">
 						<h1 class="text-4xl font-bold text-white">Username</h1>
-						<a href="/settings/2fa" class="ml-4 p-2 rounded-lg text-white/70 hover:text-white hover:bg-white/10 transition-all" title="Security Settings">
+						<a id="settingFa" href="/settings/2fa" class="ml-4 p-2 rounded-lg text-white/70 hover:text-white hover:bg-white/10 transition-all" title="Security Settings">
 							<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
@@ -123,7 +125,7 @@ export function loadProfilePage(): HTMLElement {
 
 	let currentUser: UserProfile;
 
-	getUserProfile(routeUsername).then(user => {
+	getUserProfile(me.user.username).then(user => {
 		currentUser = user;
 		const usernameElem = div.querySelector('h1');
 		const bioElem = div.querySelector('p');
@@ -159,7 +161,7 @@ export function loadProfilePage(): HTMLElement {
 		console.error("Error loading user profile:", error);
 	});
 	
-	getUserGames(routeUsername).then(games => {
+	getUserGames(me.user.username).then(games => {
 		const history = div.querySelector('#match-history');
 		if (!history) throw Error("History div not found");
 		// loops through games and adds them
@@ -177,6 +179,7 @@ export function loadProfilePage(): HTMLElement {
 
 	// EDIT PROFILE BTN
 	const editBtn = div.querySelector('#edit-profile-btn') as HTMLButtonElement;
+	const settingFa = div.querySelector('#settingFa') as HTMLElement;
 	const modal = div.querySelector('#edit-profile-modal') as HTMLDivElement;
 	const closeBtn = div.querySelector('#close-edit-modal') as HTMLButtonElement;
 	const form = div.querySelector('#edit-profile-form') as HTMLFormElement;
@@ -250,9 +253,10 @@ export function loadProfilePage(): HTMLElement {
 	}
 
 	// MYSELF or NOT logic
-	if (paramUsername !== 'me' && paramUsername !== routeUsername) {
+	if (paramUsername !== 'me') {
 		editBtn.classList.add('hidden');
 		changeAvatarBtn.classList.add('hidden');
+		settingFa.classList.add('hidden');
 	}
 	return div;
 }
